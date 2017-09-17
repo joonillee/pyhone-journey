9cc0b1077279938e77d31c2455c832dc0bb56ef6
joonillee <joonillee@gmail.com>
2017-09-17 14:12:24 -0600

    Update assignment_1.py

--- assignment_1.py
+++ assignment_1.py
@@ -1,4 +1,4 @@
-# ICT4370 Assignment #1 Joon Lee
+# Joon Lee
 # My mentality here was to write simple code as possible, not sure if I achieved but here it is.
 # I was able to ask users questions for desired answers to print once all questions are answered
 
